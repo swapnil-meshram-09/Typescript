ccf951fd372e8a68ea97f2e28ba42e4cdfee96a0
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-30 17:32:42 +0530

    new commit

--- src/any/index.ts
+++ src/any/index.ts
@@ -5,7 +5,20 @@ let value;
 let value2: any;
 // console.log(value2);    // undefined
 
+
 let value3: any;
-value3 = '123'
+value3 = 123 
 console.log(value3);
+
+value3 = 'string'
+console.log(value3);
+
+
+
+// types checking
+
+let value4: string;
+// value4 = 123        // error
+value4 = 'string'
+// console.log(value4);
 
