b9c78adb87b982bc12b1798f4afa79e872ae0213
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-30 17:14:17 +0530

    new commit

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,2 @@
-// const number = 'hello'
-// console.log(number);
-
-const name: String = 'myName'
-// console.log(name);
-
-const num: Number = 123;
-// console.log(num);
-
-const arr: Array = [1,2,3];
-console.log(arr);
-
-
+const name: string = 'name'
+console.log(name);
--- src/union/index.ts
+++ src/union/index.ts
@@ -1,6 +1,9 @@
 let value1: string | number = 10
-console.log(value1);
+// console.log(value1);
 
 let value2: string | number = '10'
-console.log(value2);
+// console.log(value2);
+
+
+
 
