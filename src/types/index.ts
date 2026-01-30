da56c51cdf2b8336e6bb2c424bb780b880299498
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-01-30 17:16:26 +0530

    new commit

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,13 @@
 const name: string = 'name'
 // console.log(name);
 
-
 const num: number = 123
-console.log(num);
+// console.log(num);
+
+const bool1: boolean = true
+// console.log(bool1);
+
+const bool2: boolean = false
+// console.log(bool2);
+
 
