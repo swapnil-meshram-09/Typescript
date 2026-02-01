fc35589e67a225b985342be960651867c2a53e08
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-01 18:14:52 +0530

    new commit

--- src/array/index.ts
+++ src/array/index.ts
@@ -8,5 +8,23 @@ const price: Array<number> = [100,200,300]
 // console.log(price);
 
 const menu: Array<string | number> = ['foodName',100,'foodCategory',2]
-console.log(menu);
+// console.log(menu);
+
+
+
+// custom types 
+
+type product = {
+    name: string,
+    quantity: number
+}
+
+const products: product[] = [
+    {name: 'product1', quantity: 2},
+    {name: 'product2', quantity: 3},
+    {name: 'product3', quantity: 4}
+]
+
+console.log(products);
+
 
