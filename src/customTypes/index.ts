bba3f17c3bf99f43dc4da92a644015e03f11b115
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-01 18:10:48 +0530

    new commit

--- src/customTypes/index.ts
+++ src/customTypes/index.ts
@@ -3,6 +3,19 @@ let apiRequestStatus: 'Pending' | 'Failed' | 'Success' = 'Pending'
 // console.log(apiRequestStatus);
 
 apiRequestStatus = 'Success'
-console.log(apiRequestStatus);
+// console.log(apiRequestStatus);
 
 
+// custom types
+
+type product = {
+    name: string,
+    quantity: number
+}
+
+const menu: product = {
+    name: 'name', quantity:2
+}
+
+// console.log(product);     // 'product' only refers to a type, but is being used as a value here.
+console.log(menu);
