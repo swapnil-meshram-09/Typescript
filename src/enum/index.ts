7cc2691f31434c5d2c88d5c8d2ad90a734c704d5
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-02 19:14:19 +0530

    new commit

--- src/enum/index.ts
+++ src/enum/index.ts
@@ -7,5 +7,19 @@ enum clothSize {
     medium,
     large
 }
-
 console.log(clothSize);
+console.log(clothSize.large);
+
+
+enum clothSize2 {
+    // small = 0,
+    // medium = 1,
+    // large = 2
+                    // auto increment values
+    small = 1,
+    medium = 2,
+    large = 3
+}
+// console.log(clothSize2);
+
+
