6c6fab64320bdf20d34f41630b71cf4201c72639
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-03 19:38:34 +0530

    new commit

--- src/optional/index.ts
+++ src/optional/index.ts
@@ -3,8 +3,11 @@ let user: [string,number,boolean?]
 user = ['name',123]
 // console.log(user);
 
+                 // error = [isLogin: boolean?]
 let food: [name:string, price:number, boolean?]
 food = ['name', 120]
-console.log(food);
+// console.log(food);
 
+food = ['name', 120, true]
+console.log(food);
 
