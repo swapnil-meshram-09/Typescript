7d4c9c2ba0bf376c5ada9fb9fabe1b44a4764e4f
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-03 18:49:06 +0530

    new commit

--- src/object/index.ts
+++ src/object/index.ts
@@ -4,4 +4,21 @@ const user = {
     name: 'name',
     number: 123           
 }
-console.log(user);
+// console.log(user);
+
+
+
+// typescipt objects
+
+let user2: {
+    name: string,
+    number: number
+}
+
+user2 = {
+    name: 'name',
+    number: 123 
+}
+
+console.log(user2);
+
