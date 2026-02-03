422ed354f1cade4001c851bc322a81b5a5682214
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-03 18:56:56 +0530

    new commit

--- src/readonly/index.ts
+++ src/readonly/index.ts
@@ -1,7 +1,15 @@
 
 let user: string = 'userName'
-console.log(user);
+// console.log(user);
 
 user = 'newUserName'
-console.log(user);
+// console.log(user);
+
+
+// with readonly keyword
+
+// error = 'readonly' type modifier is only permitted on array and tuple literal types.
+
+// let number: readonly number = 123
+
 
