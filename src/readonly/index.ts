d6e31968f87b802c81a3d327a867ef3fe5901772
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-03 19:04:49 +0530

    new commit

--- src/readonly/index.ts
+++ src/readonly/index.ts
@@ -21,4 +21,17 @@ user = 'newUserName'
 
 
 let num: readonly number[] = [1,2,3]
-console.log(num);
+// console.log(num);
+
+// num = [3,2,1]
+// console.log(num);
+
+
+
+// tuple
+
+let num2: readonly [number,number,number] = [4,5,6]
+// console.log(num2);
+
+// num2 = [6,5,4]
+// console.log(num2);
