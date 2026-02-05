b6fb173533081b6f05c08930f1a70d78d6772b34
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-05 18:56:09 +0530

    new commit

--- src/class/constructor.ts
+++ src/class/constructor.ts
@@ -14,6 +14,8 @@ const user1 = new user('user',123)
 const user2 = new user('user2',789)
 
 
+
+
 // another way
 
 class menu {
@@ -24,18 +26,11 @@ class menu {
     constructor(){
         // console.log(food);   //error
 
-        console.log(this.food);    // foodName
+        // console.log(this.food);    // foodName
     }
 }
 const menu1 = new menu()
 // console.log(menu1);         // menu { food: 'foodName', price: 120 }
-
-
-
-
-
-
-
 
 
 
---
+++ src/class/extends.ts
@@ -0,0 +1,14 @@
+
+class vehicle{
+    category:string = 'bike'   
+    constructor(){
+
+    } 
+}
+
+class bike extends vehicle {
+     
+}
+
+const vehicle1 = new vehicle()
+const bike1 = new bike()
