d458686550cbdfa8eb013da804856b67b574f238
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-05 18:33:58 +0530

    new commit

---
+++ src/class/class.ts
@@ -0,0 +1,19 @@
+// classes and objects
+
+class user{
+    name:string = 'name'
+    number:number = 123
+
+    print(): void{
+        // console.log(name);    // 'name' is deprecated.
+        console.log(this.name);
+        
+    } 
+}
+
+const user1 = new user()   
+console.log(user1);             // user { name: 'name', number: 123 }
+console.log(user1.print);       // [Function: print]
+console.log(user1.print());     // name & undefined
+console.log(user1.name);        // name
+
