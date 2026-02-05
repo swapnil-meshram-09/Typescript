2a025ef849dba5dc19bcf9a68a2d6852c2c34b6a
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-05 19:08:26 +0530

    new commit

--- src/class/public.ts
+++ src/class/public.ts
@@ -3,5 +3,8 @@ class user{
     public name:string = 'name'
     // console.log();    // error =>  Function implementation is missing or not immediately following the declaration.ts(2391)'log', which lacks return-type annotation, implicitly has an 'any' return type
     
+    constructor(){
+        console.log(this.name);
+    }
 }
 
