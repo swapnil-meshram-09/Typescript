833fbe254a7115d17ad828967ce52d1d99f06e29
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-05 19:04:20 +0530

    new commit

--- src/class/abstract.ts
+++ src/class/abstract.ts
@@ -2,4 +2,9 @@
 abstract class user {
     username:string = 'name'
     abstract password:string
+    // abstract password:string = ''    // error => Property 'password' cannot have an initializer because it is marked abstract.
 }
+
+// const user1 = new user()    // error => Cannot create an instance of an abstract class.
+
+
