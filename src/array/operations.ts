9b788449d2e58456ecb954a73c8b13d3e4b4c985
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-07 15:33:08 +0530

    new commit

--- src/array/operations.ts
+++ src/array/operations.ts
@@ -2,8 +2,13 @@
 const num: number[] = [1,2,3]
 
 num.push(5,6,7)
-console.log(num);
+// console.log(num);
 
 num.pop()
-console.log(num);
+// console.log(num);
+
+// console.log(num[5]);   // undefined
+
+// console.log(num[4]);      // 6
+
 
