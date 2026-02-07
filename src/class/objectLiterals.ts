6825512cb676dfc0285b1ceffd2462ff4444f148
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-07 15:05:25 +0530

    new commit

---
+++ src/class/objectLiterals.ts
@@ -0,0 +1,13 @@
+const obj1 = {
+    name: 'name',
+    number: '123',
+    call: function(){
+        console.log(this.name); 
+    }
+}
+
+// console.log(obj1);
+// console.log(obj1.name);
+// console.log(obj1.call);
+// console.log(obj1.call());
+ 
