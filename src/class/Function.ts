905c5c734b9b677cdceda159c2824c0fca41c0ed
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-07 15:10:36 +0530

    new commit

---
+++ src/class/Function.ts
@@ -0,0 +1,7 @@
+function obj1(){
+    const name: string = 'name'
+    const number: number = 123
+    console.log(name);
+}
+
+obj1()
--- src/class/objectLiterals.ts
+++ src/class/objectLiterals.ts
@@ -10,4 +10,7 @@ const obj1 = {
 // console.log(obj1.name);
 // console.log(obj1.call);
 // console.log(obj1.call());
+
+
+// obj1['price'] = 123   // error
  
