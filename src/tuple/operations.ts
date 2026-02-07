ef74a42c8d54db671974425a2b9b2ac6f774b3b2
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-07 15:31:41 +0530

    new commit

--- src/tuple/operations.ts
+++ src/tuple/operations.ts
@@ -1,9 +1,13 @@
 
-const tup1: [number,number,number] = [1,2,3]
+const tup1: [number,number,number] = [1,2,3]     // error => [1,2,3,4]
 
 tup1.push(5,6,7)
 // console.log(tup1);
 
 
 tup1.pop()
-console.log(tup1);
+// console.log(tup1);
+
+// console.log(tup1[2]);   // 3
+// console.log(tup1[3]);      // 5 but error=> Tuple type '[number, number, number]' of length '3' has no element at index '3'.
+
