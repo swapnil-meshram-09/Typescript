d04f7bee2deb658e0229ab20c4dab387a4537967
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-07 15:12:47 +0530

    new commit

--- src/class/Function.ts
+++ src/class/Function.ts
@@ -1,7 +1,12 @@
 function obj1(){
     const name: string = 'name'
-    const number: number = 123
+    const num: number = 123
     console.log(name);
 }
+// obj1()
 
-obj1()
+
+function obj2(name:string, num:number){
+    console.log(name); 
+}
+// obj2('name',123)
