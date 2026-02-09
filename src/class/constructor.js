e8be36b73338cbcc69cada2e034141a8c0525576
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-02-09 22:47:43 +0530

    new commit

--- src/class/constructor.js
+++ src/class/constructor.js
@@ -1,11 +1,11 @@
 
 class user {
-    constructor(user:string, number:number){
+    // constructor(user:string, number:number){
         // console.log(user);    // user
 
         // console.log(this.number);  // error => Property 'number' does not exist on type 'user'.
     }
-}
+// }
 
 const user1 = new user('user',123)
 // console.log(user1);         // user {}
@@ -19,8 +19,8 @@ const user2 = new user('user2',789)
 // another way
 
 class menu {
-    food: string = 'foodName'
-    price: number = 120
+    // food: string = 'foodName'
+    // price: number = 120
     
     // when object init = constructor automatically called.
     constructor(){
@@ -34,3 +34,17 @@ const menu1 = new menu()
 
 
 
+// javascript
+class user3{
+    constructor(name, num){
+        this.name = name;
+        this.num = num;
+    }
+    print(){
+        this.name
+        this.num
+    }
+
+
+}
+const users3 = new user3('name',123)
